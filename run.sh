b484476969070de705733aa3f4e9698e422bebee
SergeyCherepanov <sergey@cherepanov.org.ua>
2016-10-02 02:03:32 +0400

    multi-version update, revert to mysql 5.6

--- run.sh
+++ run.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
+
 if [[ $(whoami) == "root" ]]; then
     echo "!!! Executing under root will destruct your system !!! Stopping..."
     exit 1
 fi
 
-# Initialize script dir
-pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
-echo ""
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "!!!!! WARNING !!!!! Please finish XCODE installation before continue !!!!!"
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo ""
-xcode-select --install
-sudo easy_install pip
-#sudo pip install ansible --quiet
-sudo pip install --ignore-installed --upgrade ansible
-ansible-playbook -v --ask-sudo-pass -i "localhost," -c local "$DIR/main.yml"
+xcode-select -p >/dev/null 2>&1 || {
+  # Initialize script dir
+  echo ""
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "!!!!! WARNING !!!!! Please finish XCODE installation before continue !!!!!"
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  echo ""
+  xcode-select --install
+}
+
+ansible --version >/dev/null 2>&1 || {
+  sudo easy_install pip
+  sudo pip install --ignore-installed --upgrade ansible
+}
+
+ansible-playbook --ask-sudo-pass -i "localhost," -c local "$DIR/main.yml" $@
