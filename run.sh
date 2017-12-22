15ea597611b9199479b9b95b1d07ceb675460780
scherepanov <sergey@cherepanov.org.ua>
2017-12-23 02:08:08 +0400

    fix sudo pass

--- run.sh
+++ run.sh
@@ -19,4 +19,4 @@ ansible --version >/dev/null 2>&1 \
   sudo sh -c "easy_install pip && pip install --ignore-installed --upgrade ansible"
 }
 
-sudo -u ${MAC_USER} ansible-playbook --ask-become-pass -i "localhost," -c local "$DIR/main.yml" -e "mac_user=${MAC_USER}" "$@"
+sudo -u ${MAC_USER} ansible-playbook -i "localhost," -c local "$DIR/main.yml" -e "mac_user=${MAC_USER}" "$@"
