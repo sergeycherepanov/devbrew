f91bbc8146ab5c7ffe5be1589ad39e97070ebbfa
scherepanov <sergey@cherepanov.org.ua>
2018-05-09 03:17:23 +0400

    python

--- run.sh
+++ run.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
-pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
+  #!/bin/bash
+  pushd `dirname $0` > /dev/null;DIR=`pwd -P`;popd > /dev/null
 
-MAC_USER=${SUDO_USER-${USER}}
-MIN_ANSIBLE_VERSION="2.4.1"
-PYTHON_VERSION="2.7.15"
-PYTHON_PKG="python-2.7.15-macosx10.9.pkg"
+  MAC_USER=${SUDO_USER-${USER}}
+  MIN_ANSIBLE_VERSION="2.4.4.0"
+  PYTHON_VERSION="2.7.15"
+  PYTHON_PKG="python-2.7.15-macosx10.9.pkg"
 
-if [[ ! -f /usr/local/bin/python ]]; then
+  if ! php -r "version_compare('"$(python --version 2>&1 | awk '{print $2}')"', '${PYTHON_VERSION}', '>=') ? exit(0) : exit(1);" || ! which pip > /dev/null; then
     curl -o "/tmp/${PYTHON_PKG}" "https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_PKG}"
     sudo installer -pkg "/tmp/${PYTHON_PKG}" -target /
-fi
+  fi
 
-# Install ansible if not found or upgrade if outdated
-ansible --version >/dev/null 2>&1 \
-&& php -r "version_compare('"$(ansible --version  | head -1 | awk '{print $2}')"', '${MIN_ANSIBLE_VERSION}', '>=') ? exit(0) : exit(1);" || {
-  sudo pip install --ignore-installed --upgrade ansible
-}
+  cd $(dirname $(which python)); cd $(dirname $(readlink $(which python)))
 
-sudo -u ${MAC_USER} ansible-playbook -i "localhost," -c local "$DIR/main.yml" -e "mac_user=${MAC_USER}" "$@"
+  # Install ansible if not found or upgrade if outdated
+  ./ansible --version >/dev/null 2>&1 \
+  && php -r "version_compare('"$(./ansible --version  | head -1 | awk '{print $2}')"', '${MIN_ANSIBLE_VERSION}', '>=') ? exit(0) : exit(1);" || {
+    sudo -H ./pip install --force-reinstall --upgrade ansible==${MIN_ANSIBLE_VERSION}
+  }
+
+  sudo -H -u ${MAC_USER} ./ansible-playbook -i "localhost," -c local "${DIR}/main.yml" -e "mac_user=${MAC_USER}" "$@"
