70df0ee5b4cb4c41d496d3952bba0871b52ee353
S.Cherepanov <sergey@cherepanov.org.ua>
2022-03-22 04:28:22 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ MIN_PYTHON_VERSION_MACOS="2.7.15"
 MIN_PYTHON_VERSION_LINUX="2.7.15"
 PYTHON_PKG_LINUX="Python-${MIN_PYTHON_VERSION_LINUX}.tgz"
 PYTHON_PKG_MACOS="python-${MIN_PYTHON_VERSION_MACOS}-macosx10.9.pkg"
-PYTHON_BIN=${PYTHON_BIN:-$(which python)}
-PYTHON_BIN=${PYTHON_BIN:-$(which python3)}
+PYTHON_BIN=${PYTHON_BIN:-$(which python > /dev/null && which python || true)}
+PYTHON_BIN=${PYTHON_BIN:-$(which python3 > /dev/null && which python3 || echo "/usr/bin/python")}
 ANSIBLE_PLAYBOOK_CMD="${PYTHON_BIN} ${DIR}/ansible-playbook"
 
 if [[ $(id -u ${MAC_USER}) -eq 0 ]]; then
